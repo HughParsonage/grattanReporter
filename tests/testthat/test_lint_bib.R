883f6132d37ca517392b4b4a25c64c1ccd475f2f
HughParsonage <hugh.parsonage@gmail.com>
2017-05-06 13:25:19 +1000

    Standardize entry types; add tests

--- tests/testthat/test_lint_bib.R
+++ tests/testthat/test_lint_bib.R
@@ -7,3 +7,10 @@ test_that("Correctly tidies known input", {
                    readLines("lint_bib_out_correct.bib", encoding = "UTF-8"))
 })
 
+test_that("Correctly changes entry type and removes bad fields", {
+  if (file.exists("./lint-bib/entry-type-out.bib")) file.remove("./lint-bib/entry-type-out.bib")
+  lint_bib(bib_file = "./lint-bib/entry-type-in.bib", outfile = "./lint-bib/entry-type-out.bib")
+  expect_identical(readLines("./lint-bib/entry-type-correct.bib", encoding = "UTF-8"),
+                   readLines("./lint-bib/entry-type-out.bib", encoding = "UTF-8"))
+})
+
