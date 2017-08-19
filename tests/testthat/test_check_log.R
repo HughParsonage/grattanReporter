b4f0cb113911ac300cf562c73455f23bfc14fbe0
HughParsonage <hugh.parsonage@gmail.com>
2017-08-19 19:16:36 +1000

    Needs to be skipped on travis; rely on grattex

--- tests/testthat/test_check_log.R
+++ tests/testthat/test_check_log.R
@@ -1,7 +1,7 @@
 context("check log")
 
 test_that("Errors when encountering a straddling page reference", {
-  # skip_on_travis()
+  skip_on_travis()
   expect_error(checkGrattanReport("check-log", compile = TRUE, pre_release = TRUE),
                regexp = "Unstable Vref")
 })
