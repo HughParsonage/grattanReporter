1d58a198b51a9ed37bcd1a01fc365d462bc44c0a
HughParsonage <hugh.parsonage@gmail.com>
2017-08-19 18:40:16 +1000

    Try this travis

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,8 @@
 library(testthat)
+if (!requireNamespace("TeXCheckR", quietly = TRUE) || packageVersion("TeXCheckR") < package_version("0.2.1")) {
+  requireNamespace("devtools", quietly = TRUE) || install.packages("devtools")
+  devtools::install_github("HughParsonage/TeXCheckR")
+}
 library(grattanReporter)
 
 test_check("grattanReporter")
