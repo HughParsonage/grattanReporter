27d0f7f4d46bffe33f24efa2c852736608141db9
HughParsonage <hugh.parsonage@gmail.com>
2017-08-23 00:05:06 +1000

    [Travis] skip on travis

--- tests/testthat/test-check_smallbox_caption_positions.R
+++ tests/testthat/test-check_smallbox_caption_positions.R
@@ -1,6 +1,7 @@
 context("Smallbox captions")
 
 test_that("Errors when smallbox occurs near chapter", {
+  skip_on_travis()
   expect_error(checkGrattanReport("./check-smallbox", compile = TRUE),
                regexp = "Smallbox.*intrudes on chapter heading")
 })
