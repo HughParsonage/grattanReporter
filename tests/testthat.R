dd7fb2f1113357be7956731f1fd7d519b0183850
HughParsonage <hugh.parsonage@gmail.com>
2017-09-01 01:20:31 +1000

    Conditional install of TeXCheckR

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,7 @@
 library(testthat)
+if (!requireNamespace("TeXCheckR", quietly = TRUE) || packageVersion("TeXCheckR") < package_version("0.2.2")) {
+  devtools::install_github('hughparsonage/TeXCheckR')
+}
 library(grattanReporter)
 
 test_check("grattanReporter")
