42dd9c50bd250dede8c0a32f9660be03c27b2f24
HughParsonage <hugh.parsonage@gmail.com>
2017-09-01 01:49:34 +1000

    Update hutils

--- tests/testthat.R
+++ tests/testthat.R
@@ -4,6 +4,11 @@ if (requireNamespace("devtools", quietly = TRUE)) {
         packageVersion("TeXCheckR") >= package_version("0.2.2"))) {
     devtools::install_github('hughparsonage/TeXCheckR')
   }
+  
+  if (!(requireNamespace("hutils", quietly = TRUE) && 
+        packageVersion("hutils") >= package_version("0.6.0"))) {
+    devtools::install_github('hughparsonage/hutils')
+  }
   library(grattanReporter)
   
   test_check("grattanReporter")
