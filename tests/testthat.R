95a4d934eaf963eb1b0b15f91fb8ef160c2a3f2a
HughParsonage <hugh.parsonage@gmail.com>
2017-09-01 01:44:56 +1000

    Add TeXCheckR

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,6 +1,7 @@
 library(testthat)
 if (requireNamespace("devtools", quietly = TRUE)) {
-  if (!requireNamespace("TeXCheckR", quietly = TRUE) || packageVersion("TeXCheckR") < package_version("0.2.2")) {
+  if (!(requireNamespace("TeXCheckR", quietly = TRUE) && 
+        packageVersion("TeXCheckR") >= package_version("0.2.2"))) {
     devtools::install_github('hughparsonage/TeXCheckR')
   }
   library(grattanReporter)
