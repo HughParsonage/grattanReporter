828515f835f50291a03b5a4e2ecb43e7a7fc4ab1
HughParsonage <hugh.parsonage@gmail.com>
2017-09-20 23:30:48 +1000

    Force on grattex. Put everything in src

--- R/setup_travis.R
+++ R/setup_travis.R
@@ -5,14 +5,14 @@
 
 setup_travis <- function() {
   travis_cache <- "/home/travis/texlive/texmf-var/web2c/tlmgr.log"
-  if (!file.exists(travis_cache) ||
+  if (!file.exists(travis_cache) || T ||
       as.double(difftime(Sys.time(), file.mtime(travis_cache), units = "days")) > 30) {
     system("source ./travis/texlive.sh")
-    system("tlmgr install acronym bigfoot blindtext chngcntr cmap nowidow mdframed navigator needspace tablefootnote tocloft xpatch multirow bbding mathastext isomath relsize")
-    system("tlmgr update --all")
-    system("sudo apt-get update")
-    system("sudo apt-get install texlive-bibtex-extra")
-    # system('wget "https://sourceforge.net/projects/biblatex-biber/files/biblatex-biber/current/binaries/Linux/biber-linux_x86_64.tar.gz"')
+    # system("tlmgr install acronym bigfoot blindtext chngcntr cmap nowidow mdframed navigator needspace tablefootnote tocloft xpatch multirow bbding mathastext isomath relsize")
+    # system("tlmgr update --all")
+    # system("sudo apt-get update")
+    # system("sudo apt-get install texlive-bibtex-extra")
+    # # system('wget "https://sourceforge.net/projects/biblatex-biber/files/biblatex-biber/current/binaries/Linux/biber-linux_x86_64.tar.gz"')
     # system('tar xzf biber-linux_x86_64.tar.gz')
     # system('export PATH=$PATH:$PWD')
     # system('tlmgr update biber')
