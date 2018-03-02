7aaf3d829a9bf8a180ca28f7f58ff297939197c2
HughParsonage <hugh.parsonage@gmail.com>
2018-03-03 00:16:24 +1100

    Fix for boxes

--- R/compare_xrefs_to_anchors.R
+++ R/compare_xrefs_to_anchors.R
@@ -1,5 +1,7 @@
 
-compare_xrefs_to_anchors <- function(path = ".", pattern = NULL, float = "figure") {
+compare_xrefs_to_anchors <- function(path = ".", 
+                                     pattern = NULL,
+                                     float = c("figure", "table", "boxe")) {
   get_wd <- getwd()
   setwd(path)
   on.exit(setwd(get_wd))
@@ -20,6 +22,12 @@ compare_xrefs_to_anchors <- function(path = ".", pattern = NULL, float = "figure
     setnames(2L, "AUX") %>%
     .[]
   
+  float <- match.arg(float)
+  lab <- 
+    switch(float,
+           "figure" = "fig",
+           "table" = "tbl",
+           "boxe"  = "box")
   
   
   figure_xrefs <-
@@ -31,11 +39,12 @@ compare_xrefs_to_anchors <- function(path = ".", pattern = NULL, float = "figure
     .[, .(caption_id, xref_page = as.integer(page))]
   
   first_figure_xref <- 
-    figure_xrefs[, .(xref_page = min(xref_page)), by = "caption_id"]
+    figure_xrefs[grepl(sprintf("[%s]", lab), caption_id, fixed = TRUE),
+                 .(xref_page = min(xref_page)), by = "caption_id"]
   
   figure_captions <-
     aux_contents %>%
-    .[grep("\\\\newlabel\\{fig[:][^@\\}]++\\}", AUX, perl = TRUE)] %>%
+    .[grep(paste0("\\\\newlabel\\{", lab, "[:][^@\\}]++\\}"), AUX, perl = TRUE)] %>%
     .[, c("fig_no", "page_no", "caption", "href", "SINK") := tstrsplit(extract_LaTeX_argument(AUX, "newlabel", n = 2L)[["extract"]], split = "}{", fixed = TRUE)] %>%
     .[, fig_no := sub("^\\{", "", fig_no, perl = TRUE)] %>%
     .[, caption := trimws(sub("\\relax", "", caption, fixed = TRUE))] %>%
@@ -44,16 +53,23 @@ compare_xrefs_to_anchors <- function(path = ".", pattern = NULL, float = "figure
   
   figure_locations <- 
     aux_contents %>%
-    .[grep("\\\\newlabel\\{fig[:][^@]++@cref\\}", AUX, perl = TRUE)] %>%
+    .[grep(paste0("\\\\newlabel\\{", lab, "[:][^@]++@cref\\}"), AUX, perl = TRUE)] %>%
     .[, caption_id := sub("^\\\\newlabel\\{([^@]++)@.*+$", "\\1", AUX, perl = TRUE)] %>%
-    .[, fig_no := sub("^.*\\[figure\\]\\[([12]?[0-9])\\]\\[([1-9])\\].*$", 
+    .[, fig_no := sub(paste0("^.*\\{\\[",
+                             float,
+                             if (float == "boxe") {
+                               "\\]\\[([1-9][0-9]*)\\].*$"
+                             } else {
+                               "\\]\\[([12]?[0-9])\\]\\[([1-9])\\].*$"
+                             }), 
                       "\\2.\\1",
                       AUX,
                       perl = TRUE)] %>%
     .[, page := sub("^.*[^0-9]([0-9]+)\\}\\}$", "\\1", AUX, perl = TRUE)] %>%
     .[, .(caption_id, fig_page = as.integer(page), fig_no)]
   
-  figure_locations[first_figure_xref, on = "caption_id", nomatch=0L] %>% 
+  p <- 
+    figure_locations[first_figure_xref, on = "caption_id", nomatch=0L] %>% 
     .[figure_captions, on = "fig_no"] %>%
     .[, rel_page := xref_page - fig_page] %>% 
     .[abs(rel_page) > 5L, label := substr(paste(fig_no, caption), 0, 50)] %>%
@@ -61,10 +77,13 @@ compare_xrefs_to_anchors <- function(path = ".", pattern = NULL, float = "figure
     geom_point() + 
     geom_label_repel(na.rm = TRUE)
   
+  print(p)
+  
   
   
   setwd(get_wd)
-  
+  figure_locations[first_figure_xref, on = "caption_id", nomatch=0L] %>% 
+    .[figure_captions, on = "fig_no"]
 }
 
 
