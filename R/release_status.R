99dd2ff4df3517a98098bcd2f0a3e744b95b8075
Carmela Chivers <Carmela.Chivers@grattaninstitute.edu.au>
2018-04-18 17:53:15 +1000

    Fix trailing whitespace issue and formatting

--- R/release_status.R
+++ R/release_status.R
@@ -6,14 +6,10 @@ release_status <- function(filename) {
   lines_before_begin_document <-
     lines[1:grep("\\begin{document}", lines, fixed = TRUE)]
 
-  if(any(grepl("^[%]\\s*[Rr]elease(:| [=])\\s*[Tt]rue$", lines_before_begin_document))){
-    c("release", "pre_release", "compile")}
-  else{
-    if(any(grepl("^[%]\\s*[Pp]re_[Rr]elease(:| [=])\\s*[Tt]rue$", lines_before_begin_document))){
-      c("pre_release", "compile")}
-    else{
-      if(any(grepl("^[%]\\s*[Cc]ompile(:| [=])\\s*[Tt]rue$", lines_before_begin_document))){
-        "compile"}
-    }
-  }
+  if(any(grepl("^[%]\\s*[Rr]elease(:| [=])\\s*[Tt]rue\\s*$", lines_before_begin_document))){
+    c("release", "pre_release", "compile")
+    } else {if(any(grepl("^[%]\\s*[Pp]re_[Rr]elease(:| [=])\\s*[Tt]rue\\s*$", lines_before_begin_document))){
+    c("pre_release", "compile")
+    } else {if(any(grepl("^[%]\\s*[Cc]ompile(:| [=])\\s*[Tt]rue\\s*$", lines_before_begin_document))){
+          "compile"}}}
 }
