39896458d7a555a17a5b559c81f892733a07dc81
HughParsonage <hugh.parsonage@gmail.com>
2018-04-18 18:04:45 +1000

    Use consistent if .. else style

--- R/release_status.R
+++ R/release_status.R
@@ -2,14 +2,15 @@
 
 release_status <- function(filename) {
   lines <- read_lines(filename)
-  
+
   lines_before_begin_document <-
     lines[1:grep("\\begin{document}", lines, fixed = TRUE)]
 
-  if(any(grepl("^[%]\\s*[Rr]elease(:| [=])\\s*[Tt]rue\\s*$", lines_before_begin_document))){
+  if (any(grepl("^[%]\\s*[Rr]elease(:| [=])\\s*[Tt]rue\\s*$", lines_before_begin_document))) {
     c("release", "pre_release", "compile")
-    } else {if(any(grepl("^[%]\\s*[Pp]re_[Rr]elease(:| [=])\\s*[Tt]rue\\s*$", lines_before_begin_document))){
+  } else if (any(grepl("^[%]\\s*[Pp]re_[Rr]elease(:| [=])\\s*[Tt]rue\\s*$", lines_before_begin_document))) {
     c("pre_release", "compile")
-    } else {if(any(grepl("^[%]\\s*[Cc]ompile(:| [=])\\s*[Tt]rue\\s*$", lines_before_begin_document))){
-          "compile"}}}
+  } else if (any(grepl("^[%]\\s*[Cc]ompile(:| [=])\\s*[Tt]rue\\s*$", lines_before_begin_document))) {
+    "compile"
+  }
 }
