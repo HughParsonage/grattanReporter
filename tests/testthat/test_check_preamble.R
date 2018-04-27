671ca2a8456898206efa8c5a2e756fa02513ac4e
HughParsonage <hugh.parsonage@gmail.com>
2018-04-28 02:39:45 +1000

    Correct logic

--- tests/testthat/test_check_preamble.R
+++ tests/testthat/test_check_preamble.R
@@ -7,10 +7,16 @@ test_that("todonotes even if disabled not acceptable", {
 })
 
 test_that("Working paper inconsistency", {
-  expect_error(check_preamble("./check-preamble/working-paper/working-paper-with-report-not.tex"), 
+  expect_error(check_preamble("./check-preamble/working-paper/working-paper-with-report-not.tex"),
                regexp = "Working paper / Report inconsistency")
-  expect_error(check_preamble("./check-preamble/working-paper/report-with-working-paper.tex"), 
+  expect_error(check_preamble("./check-preamble/working-paper/report-with-working-paper.tex"),
                regexp = "ReportOrWorkingPaper not set to.*Working Paper")
-  expect_error(check_preamble("./check-preamble/working-paper/almost-working-paper.tex"), 
+  expect_error(check_preamble("./check-preamble/working-paper/almost-working-paper.tex"),
                regexp = "Working paper / Report inconsistency")
 })
+
+test_that("Background paper ok", {
+  expect_null(check_preamble("./check-preamble/background-paper/background-paper.tex",
+                             pre_release = TRUE))
+})
+
