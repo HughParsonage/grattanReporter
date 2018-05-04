410980dc3934764eb6e9285272f67ec2644d0921
Carmela Chivers <Carmela.Chivers@grattaninstitute.edu.au>
2018-05-04 17:34:31 +1000

    test release_status with no directive present

--- tests/testthat/test_release_status.R
+++ tests/testthat/test_release_status.R
@@ -4,19 +4,23 @@ test_that("returns compile when present", {
   expect_equal(any(grepl("compile", release_status("release-status-compile.tex"))), TRUE)
 })
 
-#only false when compile is present but is not returned. Not false if compile not present but is returned.
-
 test_that("returns pre_release when present", {
   expect_equal(any(grepl("pre_release", release_status("release-status-compile.tex"))), TRUE)
 })
 
 test_that("returns release when present", {
-  expect_equal(any(grepl("^[release]", release_status("release-status-compile.tex"))), TRUE)
+  expect_equal(any(grepl("^release", release_status("release-status-compile.tex"))), TRUE)
 })
 
 test_that("Release even if trailing whitespace", {
   expect_equal(release_status("release-status-compile-ws.tex"), "compile")
 })
+
+test_that("returns FALSE when no directive present", {
+  expect_equal(release_status("release-status-no-directive.tex"), FALSE)
+})
+
+
 
 
 
