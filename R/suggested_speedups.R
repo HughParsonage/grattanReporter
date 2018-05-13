3b8f1ba23a01770bb27ec7474f803760fd166b4b
HughParsonage <hugh.parsonage@gmail.com>
2018-05-13 23:29:06 +1000

    Update suggested_speedups.R

--- R/suggested_speedups.R
+++ R/suggested_speedups.R
@@ -1,7 +1,7 @@
 
 read_lines <- function(...) {
   if (requireNamespace("readr", quietly = TRUE)) {
-    readr::read_lines(...)
+    tryCatch(readr::read_lines(...), error = function(e) {dir(dirname(..1)); stop(e$m)})
   } else {
     readLines(...)
   }
