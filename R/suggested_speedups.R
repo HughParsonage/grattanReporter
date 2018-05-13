d6ff11101789c830202a6ed4bdc65dc75e62da53
HughParsonage <hugh.parsonage@gmail.com>
2018-05-13 23:36:37 +1000

    Revert "Update suggested_speedups.R"
    This reverts commit ec428465effbc6fdb12d7aef43248316af8aecf1.

--- R/suggested_speedups.R
+++ R/suggested_speedups.R
@@ -1,7 +1,7 @@
 
 read_lines <- function(...) {
   if (requireNamespace("readr", quietly = TRUE)) {
-    tryCatch(readr::read_lines(...), error = function(e) {cat(dir(dirname(..1)), sep = "\n"); stop(e$m)})
+    tryCatch(readr::read_lines(...), error = function(e) {dir(dirname(..1)); stop(e$m)})
   } else {
     readLines(...)
   }
