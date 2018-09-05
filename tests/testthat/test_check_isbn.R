2e3ad4c1c60d0952dab6b4e897294c2daadd162e
HughParsonage <hugh.parsonage@gmail.com>
2018-09-05 20:33:09 +1000

    Add coverage

--- tests/testthat/test_check_isbn.R
+++ tests/testthat/test_check_isbn.R
@@ -13,5 +13,23 @@ test_that("gets ISBN", {
 
 })
 
-
+test_that("Advises ISBN", {
+  skip_if_not_installed("rlang")
+  skip_if_not(identical(Sys.getenv("USERNAME"), "hughp"))
+  skip_if_not(dir.exists("~/grattex/") && file.exists("~/grattex/Report.tex"))
+  tempd <- tempfile()
+  dir.create(tempd)
+  expect_error(check_preamble("~/grattex/Report.tex",
+                              pre_release = TRUE),
+               regexp = "ISBN has already been used.")
+  rlang::with_options({
+    expect_output(tryCatch(check_preamble("~/grattex/Report.tex",
+                                          pre_release = TRUE),
+                           error = function(e) {
+                             NULL
+                           }),
+                  regexp = "The next ISBN is ")
+  },
+  TeXCheckR.capture.output = TRUE)
+})
 
